07061c03513e743fe4cce60c645327fcbf27330e
Ingolf Wagner <palipalo9@googlemail.com>
2014-07-08 23:19:47 +0200

    works kinda ok and is not that buggy anymore

--- src/Module/Stepper.lua
+++ src/Module/Stepper.lua
@@ -23,6 +23,7 @@ function Stepper:__init()
     self.instrument = 1
     self.note       = note.c
     self.octave     = 4
+    self.matrix     = {}
     self.color      = {
         off  = color.red,
         note = color.yellow
@@ -35,12 +36,14 @@ function Stepper:callback_set_instrument()
     return function (index)
         self.track      = index
         self.instrument = index
+        self:clear_pad_matrix()
         self:clear_matrix()
         self:update_matrix()
+        self:update_pad_matrix()
     end
 end
 
-function Stepper:clear_matrix()
+function Stepper:clear_pad_matrix()
     for y = 1, 4 do
         for x = 1,8 do
             self.pad:set_matrix(x,y,color.off)
@@ -54,6 +57,26 @@ function line_to_point(line)
     return {x,y}
 end
 
+
+function Stepper:clear_matrix()
+    self.matrix = {}
+    for x = 1, 8 do
+        self.matrix[x] = {}
+    end
+end
+
+function Stepper:update_pad_matrix()
+    for x = 1, 8 do
+        for y = 1, 4 do
+            if (self.matrix[x][y]) then
+                self.pad:set_matrix(x,y,self.matrix[x][y])
+            end
+        end
+    end
+end
+
+-- ---
+-- todo : should update an inner matrix (table) which should update the pad
 function Stepper:update_matrix()
     local pattern_iter = renoise.song().pattern_iterator
     local pattern_index = renoise.song().selected_pattern_index
@@ -66,16 +89,16 @@ function Stepper:update_matrix()
             -- note_column.note_string = arp_sequence[arp_index].note
             -- note_column.instrument_value = arp_sequence[arp_index].instrument
             -- note_column.volume_value = arp_sequence[arp_index].volume
-            print(pos.line, note_column.note_string)
+            -- print(pos.line, note_column.note_string)
             if(note_column.note_string ~= "---") then
                 local xy = line_to_point(pos.line)
                 local x = xy[1]
                 local y = xy[2]
                 if (y < 5) then
                     if (note_column.note_string == "OFF") then
-                        self.pad:set_matrix(x,y,self.color.off)
+                        self.matrix[x][y] = self.color.off
                     else
-                        self.pad:set_matrix(x,y,self.color.note)
+                        self.matrix[x][y] = self.color.note
                     end
                 end
             end
@@ -85,11 +108,21 @@ end
 
 
 function Stepper:_activate()
+    self:clear_matrix()
+    self:update_matrix()
+    self:clear_pad_matrix()
+    self:update_pad_matrix()
     self.f = function (line) self:callback_playback_position(line) end
     self.playback_position_observer:register(self.f)
 end
 
-
+function Stepper:set_old_pad(x,y)
+    if(self.matrix[x][y]) then
+        self.pad:set_matrix(x,y,self.matrix[x][y])
+    else
+        self.pad:set_matrix(x,y,color.off)
+    end
+end
 
 -- ------------------------------------------------------------
 -- set the stepper to the line
@@ -100,11 +133,14 @@ function Stepper:callback_playback_position(line)
     local y = xy[2]
     if (x < 9 and y < 5) then
         if (x == 1 and y == 1) then
-            self.pad:set_matrix(8,4,color.off)
+            self:set_old_pad(8,4)
+            --self.pad:set_matrix(8,4,color.off)
         elseif (x == 1) then
-            self.pad:set_matrix(8, y - 1, color.off)
+            self:set_old_pad(8,y-1)
+            --self.pad:set_matrix(8, y - 1, color.off)
         else
-            self.pad:set_matrix(x - 1 , y , color.off)
+            self:set_old_pad(x-1,y)
+            --self.pad:set_matrix(x - 1 , y , color.off)
         end
         self.pad:set_matrix(x,y,color.green)
     end
