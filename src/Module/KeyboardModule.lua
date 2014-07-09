9e2ac2e7b0da2378851608d04b3a8f971930ba2c
Ingolf Wagner <palipalo9@googlemail.com>
2014-07-09 20:50:40 +0200

    also use pitch everywhere

--- src/Module/KeyboardModule.lua
+++ src/Module/KeyboardModule.lua
@@ -221,10 +221,9 @@ end
 function KeyboardModule:trigger_note()
     local OscMessage = renoise.Osc.Message
     local track      = self.instrument
-    local pitch      = self.note[access.pitch]
+    local tone       = self.note[access.pitch]
     local velocity   = 127
-    print(("pitch : %s"):format(pitch))
-    if pitch == -1 then
+    if tone == -1 then
         -- todo make this turn of the notes
         self.client:send(OscMessage("/renoise/trigger/note_off",{
             {tag="i",value=self.instrument},
@@ -236,7 +235,7 @@ function KeyboardModule:trigger_note()
         self.client:send(OscMessage("/renoise/trigger/note_on",{
             {tag="i",value=self.instrument},
             {tag="i",value=track},
-            {tag="i",value=(pitch + (self.octave * 12))},
+            {tag="i",value=pitch(self.note,self.octave)},
             {tag="i",value=velocity}}))
     end
 end
