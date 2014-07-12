04e3f489a460b6c0e3582fd36395ee943984b0b5
Ingolf Wagner <palipalo9@googlemail.com>
2014-07-12 19:01:49 +0200

    chooser works now fine (tracks have to be created on demand)

--- src/Module/Chooser.lua
+++ src/Module/Chooser.lua
@@ -21,24 +21,56 @@ function Chooser:__init()
     LaunchpadModule:__init(self)
     self.active      = 1  -- active instrument index
     self.row         = 6
-    self.inst_offset = 0  -- which is the first instrument
     self.color = {
         active  = color.flash.green,
         passive = color.green,
+        page = {
+            active   = color.yellow,
+            inactive = color.off,
+        }
     }
+    -- page
+    self.page         = 1
+    self.page_inc_idx = 4
+    self.page_dec_idx = 3
+    self.inst_offset  = 0  -- which is the first instrument
     -- callbacks
     self.callback_select_instrument = {}
 end
 
 function Chooser:_activate()
-    self:update_row()
-    self:matrix_callback()
-    self:top_callback()
 
-    -- notifier to update row
+    -- chooser line
+    local function matrix_listener(_,msg)
+        if msg.vel == 0x00    then return end
+        if msg.y  ~= self.row then return end
+        self:select_instrument(msg.x)
+        self:update_row()
+    end
+    self.pad:register_matrix_listener(matrix_listener)
     renoise.song().instruments_observable:add_notifier(function (_)
         self:update_row()
     end)
+    self:update_row()
+
+    -- page logic
+    local function page_knobs_listener(_,msg)
+        if (msg.vel == 0) then return end
+        if (msg.x == self.page_inc_idx) then
+            self:page_inc()
+            self:page_update_knobs()
+            self:update_row()
+        elseif (msg.x == self.page_dec_idx) then
+            self:page_dec()
+            self:page_update_knobs()
+            self:update_row()
+        end
+    end
+    self.pad:register_top_listener(page_knobs_listener)
+    renoise.song().instruments_observable:add_notifier(function (_)
+        self:page_update_knobs()
+    end)
+    self:page_update_knobs()
 end
 
 function Chooser:select_instrument(x)
@@ -53,17 +85,35 @@ function Chooser:select_instrument(x)
     end
 end
 
-function Chooser:matrix_callback()
-    local function matrix_listener(_,msg)
-        if msg.vel == 0x00     then return end
-        if (msg.y ~= self.row) then return end
-        self:select_instrument(msg.x)
-        self:update_row()
-    end
-    self.pad:register_matrix_listener(matrix_listener)
+function Chooser:top_callback()
 end
 
-function Chooser:top_callback() end
+function Chooser:page_update_knobs()
+    local instrument_count = table.getn(renoise.song().instruments)
+    if (self.inst_offset + 8 ) < instrument_count then
+        self.pad:set_top(self.page_inc_idx, self.color.page.active)
+    else
+        self.pad:set_top(self.page_inc_idx, self.color.page.inactive)
+    end
+    if self.inst_offset >  7 then
+        self.pad:set_top(self.page_dec_idx, self.color.page.active)
+    else
+        self.pad:set_top(self.page_dec_idx, self.color.page.inactive)
+    end
+end
+
+function Chooser:page_inc()
+    local instrument_count = table.getn(renoise.song().instruments)
+    if (self.inst_offset + 8) < instrument_count then
+        self.inst_offset = self.inst_offset + 8
+    end
+end
+
+function Chooser:page_dec()
+    if( self.inst_offset > 0 ) then
+        self.inst_offset = self.inst_offset - 8
+    end
+end
 
 function Chooser:update_row()
     self:clear_row()
