a5c6c085240c54bc4f82c494ae0b41d6dfdb1e1d
Ingolf Wagner <palipalo9@googlemail.com>
2014-07-20 13:36:05 +0200

    improved note system and all

--- src/Data/Note.lua
+++ src/Data/Note.lua
@@ -18,43 +18,38 @@ Note = {
         label  = 2,
     },
     note = {
-        c   = {  0 , "C-"  , 1, 2 },
-        cis = {  1 , "C#"  , 2, 1 },
-        d   = {  3 , "D-"  , 2, 2 },
-        dis = {  4 , "D#"  , 3, 1 },
-        e   = {  5 , "E-"  , 3, 2 },
-        f   = {  6 , "F-"  , 4, 2 },
-        fis = {  7 , "F#"  , 5, 1 },
-        g   = {  8 , "G-"  , 5, 2 },
-        gis = {  9 , "G#"  , 6, 1 },
-        a   = { 10 , "A-"  , 6, 2 },
-        ais = { 11 , "A#"  , 7, 1 },
-        b   = { 12 , "B-"  , 7, 2 }, -- h
-        C   = { 13 , "C-"  , 8, 2 },
-        off = { -1 , "OFF" , 4, 1 },
+        c   = {  0  , "C-"  , 1, 2 },
+        cis = {  1  , "C#"  , 2, 1 },
+        d   = {  2  , "D-"  , 2, 2 },
+        dis = {  3  , "D#"  , 3, 1 },
+        e   = {  4  , "E-"  , 3, 2 },
+        f   = {  5  , "F-"  , 4, 2 },
+        fis = {  6  , "F#"  , 5, 1 },
+        g   = {  7  , "G-"  , 5, 2 },
+        gis = {  8  , "G#"  , 6, 1 },
+        a   = {  9  , "A-"  , 6, 2 },
+        ais = { 10  , "A#"  , 7, 1 },
+        b   = { 11  , "B-"  , 7, 2 }, -- h
+        C   = { 12  , "C-"  , 8, 2 },
+        off = { 120 , "OFF" , 4, 1 },
     },
-    off    = {
-        value = -1,
-        pitch = 120,
-    }
+    empty =   { 121 , "---" , 0, 0 },  -- just for completeness
 }
 
 
--- ---
--- calculate pitch
+--- calculate pitch
 --
--- todo remove magic numbers
 function pitch(note,octave)
     local p = note[Note.access.pitch]
-    if (p > -1) then
+    if (p < 14) then
         return p + (octave * 12)
     else
-        return Note.off.pitch
+        return p
     end
 end
 
-function is_not_off(note) return note[Note.access.pitch] ~= -1 end
-function is_off(note)     return note[Note.access.pitch] == -1 end
+function is_not_off(note) return note[Note.access.pitch] ~= Note.note.off[Note.access.pitch] end
+function is_off(note)     return note[Note.access.pitch] == Note.note.off[Note.access.pitch] end
 
 function print_tone(note,octave)
     print(("note : %s%s"):format(note[Note.access.label],octave))
