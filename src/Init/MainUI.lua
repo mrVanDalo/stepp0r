22e2e1e679dfc76ba346a05957851f16a9629bbc
Ingolf Wagner <palipalo9@googlemail.com>
2014-10-03 07:42:55 +0200

    MainUI looks ok now

--- src/Init/MainUI.lua
+++ src/Init/MainUI.lua
@@ -5,30 +5,77 @@ class "MainUI"
 
 function MainUI:__init()
     self.vb = renoise.ViewBuilder()
+    self.button_size = 40
+    self.text_size   = 100
+    self.input_size  = 150
 end
 
 function MainUI:createUI()
-    self:createLogo()
-    self:createTitle()
-    self:createContainer()
+    self:create_logo()
+    self:create_title()
+    self:create_device_row()
+    self:create_osc_row()
+    self:create_container()
 end
 
-function MainUI:createContainer()
-    self.container = self.vb:column{
-        margin = 10,
-        spacing = 10,
-        self.title,
-        self.logo,
+function MainUI:create_device_row()
+    self.device_row = self.vb:row{
+        spacing = 3,
+        self.vb:button{
+            visible = true,
+            text    = "re",
+            width   = self.button_size,
+        },
+        self.vb:text{
+            text = "Device",
+            width = self.text_size,
+        },
+        self.vb:popup {
+            width = self.input_size,
+        },
     }
 end
 
-function MainUI:createLogo()
+function MainUI:create_osc_row()
+    self.osc_row = self.vb:row{
+        spacing = 3,
+        self.vb:checkbox{
+            visible = true,
+            value   = true,
+            width   = self.button_size,
+        },
+        self.vb:text{
+            text = "OSC Port",
+            width = self.text_size,
+        },
+        self.vb:textfield {
+            text = '1234',
+            value = '555',
+            width = self.input_size,
+        }
+    }
+end
+
+function MainUI:create_container()
+    self.container = self.vb:column{
+        spacing = 8,
+        self.logo,
+        self.vb:column {
+            spacing = 4,
+            margin = 4,
+            self.osc_row,
+            self.device_row,
+        }
+    }
+end
+
+function MainUI:create_logo()
     self.logo = self.vb:bitmap{
         bitmap = "logo.png"
     }
 end
 
-function MainUI:createTitle()
+function MainUI:create_title()
     self.title = self.vb:text {
         text = "Awesome"
     }
