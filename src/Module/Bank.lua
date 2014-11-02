063bd053a7700ce36bf92abe675b657ae2d6aa69
Ingolf Wagner <palipalo9@googlemail.com>
2014-11-02 20:00:12 +0100

    on working on the bank

---
+++ src/Module/Bank.lua
@@ -0,0 +1,16 @@
+--- ======================================================================================================
+---
+---                                                 [ Bank Module ]
+-- to store Copy paste Pattern
+
+class "Bank" (Module)
+
+function Bank:__init(self)
+    Module:__init(self)
+    self.offset = 6
+    -- default
+    self.color = {
+
+    }
+end
+
