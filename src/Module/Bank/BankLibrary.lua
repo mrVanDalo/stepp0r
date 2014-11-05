b159b52f56e29178f5b87e91af49221a66c9767f
Ingolf Wagner <palipalo9@googlemail.com>
2014-11-05 20:38:37 +0100

    ui works so fare

--- src/Module/Bank/BankLibrary.lua
+++ src/Module/Bank/BankLibrary.lua
@@ -7,14 +7,14 @@ function Bank:_toggle_mode(bank_idx)
         self.mode = BankData.mode.copy
     end
     self:_update_bank_listeners()
-    self._render_matrix()
+    self:_render_matrix()
 end
 
 function Bank:_clear_bank(bank_idx)
     self.bank_idx = bank_idx
     self.mode = BankData.mode.copy
     self:_update_bank_listeners()
-    self._render_matrix()
+    self:_render_matrix()
 end
 
 
