c4bdc1ab03d13844e8f3fc0e59e7634ce8e15185
Ingolf Wagner <palipalo9@googlemail.com>
2014-12-28 13:07:53 +0100

    better naming again

--- src/Module/Chooser/ChooserMode.lua
+++ src/Module/Chooser/ChooserMode.lua
@@ -14,12 +14,12 @@ function Chooser:__init_mode()
     self:__create_mode_listener()
 end
 function Chooser:__activate_mode()
-    self:__mode_update_knobs()
-    self.pad:register_right_listener(self.mode_listener)
+    self:__update_mode_knobs()
+    self.pad:register_right_listener(self.__mode_listener)
 end
 function Chooser:__deactivate_mode()
-    self:__mode_clear_knobs()
-    self.pad:unregister_right_listener(self.mode_listener)
+    self:__clear_mode_knobs()
+    self.pad:unregister_right_listener(self.__mode_listener)
 end
 
 --- ------------------------------------------------------------------------------------------------------
@@ -27,16 +27,16 @@ end
 ---                                                 [ Lib ]
 
 function Chooser:__create_mode_listener()
-    self.mode_listener = function (_,msg)
+    self.__mode_listener = function (_,msg)
         if self.is_not_active          then return end
         if msg.vel == Velocity.release then return end
         if msg.x   ~= self.mode_idx    then return end
-        self:__mode_next()
-        self:__mode_update_knobs()
+        self:__next_mode()
+        self:__update_mode_knobs()
     end
 end
 
-function Chooser:__mode_next()
+function Chooser:__next_mode()
     if self.mode == ChooserData.mode.choose then
         self.mode = ChooserData.mode.mute
     elseif self.mode == ChooserData.mode.mute then
@@ -46,11 +46,11 @@ function Chooser:__mode_next()
     end
 end
 
-function Chooser:__mode_update_knobs()
+function Chooser:__update_mode_knobs()
     -- print(self.mode)
     self.pad:set_side(self.mode_idx, self.mode[ChooserData.access.color])
 end
-function Chooser:__mode_clear_knobs()
+function Chooser:__clear_mode_knobs()
     -- print(self.mode)
     self.pad:set_side(self.mode_idx, Color.off)
 end
