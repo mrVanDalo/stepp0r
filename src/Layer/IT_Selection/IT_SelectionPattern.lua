5a371f5c9f30110fb6797b76499f09a215973c48
Ingolf Wagner <palipalo9@googlemail.com>
2015-02-14 19:22:48 +0100

    add pattern logic in IT Selection

--- src/Layer/IT_Selection/IT_SelectionPattern.lua
+++ src/Layer/IT_Selection/IT_SelectionPattern.lua
@@ -3,25 +3,35 @@
 function IT_Selection:_init_pattern()
     self.pattern_idx = 1
     self.callback_select_pattern = {}
+    self:__create_selected_pattern_listener()
 end
 
 
 function IT_Selection:_connect_pattern()
+    add_notifier(renoise.song().selected_pattern_index_observable, self.__select_pattern_listener)
+end
 
+function IT_Selection:_disconnect_pattern()
+    remove_notifier(renoise.song().selected_pattern_index_observable, self.__select_pattern_listener)
+end
+
+--- selected pattern has changed listener
+function It_Selection:__create_select_pattern_listener()
+    self.__select_pattern_listener = function (_)
+        self.pattern_idx = renoise.song().selected_pattern_index
+        self:__update_set_pattern_listeners()
+    end
 end
 
 --- register callback
 --
 -- the callback gets the `index of pattern`
-function IT_Selection:register_select_instrument(callback)
+function IT_Selection:register_select_pattern(callback)
     table.insert(self.callback_select_pattern, callback)
 end
 
 -- function :callback_set_instrument() return function(instrument_idx, track_idx, column_idx) end end
 function IT_Selection:__update_set_pattern_listeners()
-    --    log("instrument_idx", self.instrument_idx)
-    --    log("track_idx", self.track_idx)
-    --    log("column_idx", self.column_idx)
     for _, callback in ipairs(self.callback_select_pattern) do
         callback(self.pattern_idx)
     end
