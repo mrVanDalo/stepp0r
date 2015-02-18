bb43c8204726ebfcd9a157b4eb16ca5d4b9bb9d6
Ingolf Wagner <palipalo9@googlemail.com>
2015-02-18 21:04:13 +0100

    removed todos

--- src/Module/PatternEditorModule.lua
+++ src/Module/PatternEditorModule.lua
@@ -90,14 +90,14 @@ end
 
 function PatternEditorModule:__update_pattern_matrix()
     local pattern_iter  = renoise.song().pattern_iterator
-    for pos,line in pattern_iter:lines_in_pattern_track(self.pattern_idx, self.track_idx) do -- todo missing
+    for pos,line in pattern_iter:lines_in_pattern_track(self.pattern_idx, self.track_idx) do
         self:__update_pattern_matrix_position(pos,line)
     end
 end
 function PatternEditorModule:__update_pattern_matrix_position(pos,line)
     if table.is_empty(line.note_columns) then return end
     -- get note at position
-    local note_column = line:note_column(self.track_column_idx) -- todo missing
+    local note_column = line:note_column(self.track_column_idx)
     if (note_column.note_value == PatternEditorModuleData.note.empty) then return end
     -- calculate (x,y)
     local xy = self:line_to_point(pos.line) -- todo missing
