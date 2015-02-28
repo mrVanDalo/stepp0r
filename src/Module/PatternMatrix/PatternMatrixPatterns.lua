264d02dd5cf6026a41e3668364a2d331a687bbef
Ingolf Wagner <palipalo9@googlemail.com>
2015-02-28 20:33:16 +0100

    works way better now

--- src/Module/PatternMatrix/PatternMatrixPatterns.lua
+++ src/Module/PatternMatrix/PatternMatrixPatterns.lua
@@ -5,14 +5,27 @@ function PatternMatrix:__init_patterns()
     self.pattern_mix_2_sequence_idx = nil
     self.active_mix_pattern         = nil
     self.next_mix_pattern           = nil
+    --
+    self:__create_selected_pattern_idx_listener()
 end
 
 function PatternMatrix:__activate_patterns()
     self:__set_mix_patterns()
+    self:__set_active_and_next_patterns()
+    add_notifier(renoise.song().selected_pattern_index_observable, self.__selected_pattern_idx_listener)
 end
 
 function PatternMatrix:__deactivate_patterns()
+    remove_notifier(renoise.song().selected_pattern_index_observable, self.__selected_pattern_idx_listener)
+end
 
+function PatternMatrix:__create_selected_pattern_idx_listener()
+    self.__selected_pattern_idx_listener = function ()
+        if self.is_not_active then return end
+        self:__set_active_and_next_patterns()
+        self:_clear_launchpad()
+        self:_render_matrix()
+    end
 end
 
 function PatternMatrix:_get_pattern_alias_idx(pattern,x)
@@ -36,6 +49,9 @@ function PatternMatrix:__set_mix_patterns()
         self.pattern_mix_2_sequence_idx = tupel_2[1]
         print("found pattern_mix_2 at : ", self.pattern_mix_2_sequence_idx)
     end
+end
+
+function PatternMatrix:__set_active_and_next_patterns()
     self.active_mix_pattern = self:_active_mix_pattern()
     print("found active_mix_pattern at : ", self.active_mix_pattern.name)
     self.next_mix_pattern   = self:_next_mix_pattern()
@@ -62,7 +78,7 @@ end
 
 function PatternMatrix:_active_mix_pattern()
     if (self.pattern_mix_1 and self.pattern_mix_2) then
-        if self.active_mix_pattern == self.pattern_mix_2 then
+        if renoise.song().selected_pattern == self.pattern_mix_2 then
             return self.pattern_mix_2
         else
             return self.pattern_mix_1
