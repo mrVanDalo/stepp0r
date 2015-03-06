a2974a3466d0c4beb0803487809cd48da06b1f5a
Ingolf Wagner <palipalo9@googlemail.com>
2015-03-06 21:26:06 +0100

    created them functions

--- src/Objects/TrackObject.lua
+++ src/Objects/TrackObject.lua
@@ -6,32 +6,24 @@ class 'TrackObject'
 
 function TrackObject:__init() end
 
+--- todo maybe this don't belong here
 --- @return track of given instrument index
-function TrackObject:track_for_instrument_index(instrument_number)
-    local number = self:track_index_for_instrument_index(instrument_number)
+function TrackObject:sequencer_track_for_instrument_index(instrument_number)
+    local number = self:sequencer_track_index_for_instrument_index(instrument_number)
     return renoise.song().tracks[number]
 end
 
+--- todo maybe this don't belong here
 --- @return track number of given instrument index
-function TrackObject:track_index_for_instrument_index(instrument_number)
-    -- todo finish me (i'm not working yet)
-    --    self:__ensure_track_exist(instrument_number)
-    -- calculate the correct track
-    local counter = 0
-    for index, track in pairs( renoise.song().tracks ) do
-        if track.type == renoise.Track.TRACK_TYPE_SEQUENCER
-                and track.type ~= renoise.Track.TRACK_TYPE_GROUP
-        then
-            counter = counter + 1
-        end
-        if counter == instrument_number then
-            -- log('return track_number', counter)
-            return index
-        end
-    end
+function TrackObject:sequencer_track_index_for_instrument_index(instrument_number)
+    local sequencer_track_sequence = self:sequencer_track_sequence()
+    return sequencer_track_sequence[instrument_number]
 end
 
-function TrackObject:ensure_track_idx_exist(track_idx)
+
+
+
+function TrackObject:ensure_sequencer_track_idx_exist(track_idx)
     local nr_of_tracks    = self:track_sequence_size()
     local how_many_to_add = track_idx -  nr_of_tracks
     if (how_many_to_add > 0) then
@@ -44,9 +36,10 @@ function TrackObject:ensure_track_idx_exist(track_idx)
     end
 end
 
+
 --- sequence of all tracks
 -- excluding send tracks and groups
-function TrackObject:track_sequence()
+function TrackObject:sequencer_track_sequence()
     local sequence_idx = 1
     local track_idx    = 1
     local sequence = {}
@@ -59,16 +52,14 @@ function TrackObject:track_sequence()
     end
     return sequence
 end
-
---- use this function only if you don't need the track_sequence itself.
-function TrackObject:track_sequence_size()
-    -- fixme : isn't this coverd by : renoise.song().sequencer_track_count ?
-    local sequence_idx = 1
-    for _, track in pairs( renoise.song().tracks ) do
-        if track.type == renoise.Track.TRACK_TYPE_SEQUENCER then
-            sequence_idx = sequence_idx + 1
-        end
-    end
-    return sequence_idx
+function TrackObject:sequencer_track_count()
+    return renoise.song().sequencer_track_count
 end
 
+
+function TrackObject:rename_track_index(index, name)
+    local track = renoise.song().tracks[index]
+    if track then
+        track.name = name
+    end
+end
