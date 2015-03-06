33e413ad4d00e2e812eb3480a216ead6b2d75620
Ingolf Wagner <palipalo9@googlemail.com>
2015-03-06 17:22:57 +0100

    created another object, but lets keep focused

--- src/Objects/Pool.lua
+++ src/Objects/Pool.lua
@@ -3,7 +3,8 @@ require 'Objects/PatternMatrixObject'
 require 'Objects/PatternEditorObject'
 
 Renoise = {
-    pattern_matrix = PatternMatrixObject()
+    pattern_matrix = PatternMatrixObject(),
+    pattern_editor = PatternEditorObject()
 }
 
 
