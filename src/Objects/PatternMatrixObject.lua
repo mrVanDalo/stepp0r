31d65c354597430159c907666eea08babbf66b53
Ingolf Wagner <palipalo9@googlemail.com>
2016-01-12 13:41:37 -0300

    clearing the patterns now

--- src/Objects/PatternMatrixObject.lua
+++ src/Objects/PatternMatrixObject.lua
@@ -8,7 +8,6 @@ end
 
 function PatternMatrixObject:create_patterns_up_to_sequence_index(index)
     if index < 1 then return end
-    renoise.song().sequencer:sort()
     local number_of_sequences = self:number_of_sequences()
     local to_create = index - number_of_sequences
     for counter = 1, to_create do
@@ -20,7 +19,6 @@ function PatternMatrixObject:insert_sequence_at_index(index)
     if index < 1 then return end
     local number_of_sequences = self:number_of_sequences()
     if index > number_of_sequences then return end
-    renoise.song().sequencer:sort()
     renoise.song().sequencer:insert_new_pattern_at(index)
 end
 
@@ -28,10 +26,9 @@ function PatternMatrixObject:remove_sequence_index(index)
     if index < 1 then return end
     local number_of_sequences = self:number_of_sequences()
     if index > number_of_sequences then return end
-    renoise.song().sequencer:sort()
-    -- todo : clear pattern first
     renoise.song().sequencer:delete_sequence_at(index)
 end
+
 
 
 -- todo : return nil should make it more readable
